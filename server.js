5b996b7e1e5da44acb0649700b878b91ad1cea33
Brandon Hamilton <bham2015@gmail.com>
2024-10-07 12:59:54 -0500

    Added GET logic for '/' endpoint.

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// TODO: this file :)
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello employees!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
