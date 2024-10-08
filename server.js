1c212e37ee5a39550f9ab91ab0676310691240ab
Brandon Hamilton <bham2015@gmail.com>
2024-10-07 20:27:19 -0500

    Solved for the GET /employees/:id endpoint.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = 3000;
-
+const employees = require("./employees");
 app.get("/", (req, res) => {
   res.send("Hello employees!");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.get("/employees/:id", (req, res) => {
+  const { id } = req.params;
+
+  const obj = employees.find((employee) => employee.id === +id);
+
+  if (obj) {
+    res.json(obj);
+  } else {
+    res.status(404).send("There is no employee with that id.");
+  }
 });
 
 app.listen(PORT, () => {
